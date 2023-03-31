9c4c20eaef15df68c053c85e0ae25c6fb4ce5df3
Seyyed Iman Asnafi <imisia.dev@gmail.com>
2023-03-31 20:41:37 +0430

    improve import views

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AuthenticateRoute from './middleware/AuthenticateRoute';
 import AuthorizeRoute from './middleware/AuthorizeRoute';
 
-import PanelView from '../views/PanelView.vue';
+import PanelView from '@/views/PanelView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,7 +17,7 @@ const router = createRouter({
                 {
                     path: '',
                     name: 'Dashboard',
-                    component: () => import('../views/DashboardView.vue'),
+                    component: () => import('@/views/DashboardView.vue'),
                     beforeEnter: [AuthorizeRoute],
                     meta: {
                         permissions: ['dashboard']
@@ -31,7 +31,7 @@ const router = createRouter({
             // route level code-splitting
             // this generates a separate chunk (Login.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import('../views/LoginView.vue')
+            component: () => import('@/views/LoginView.vue')
         }
     ]
 });
