610070f88dc438f750196a3ea82a601004e95171
Sajad Abasi <sab3053@gmail.com>
2023-05-26 17:27:27 +0330

    create input addon component

---
+++ src/lib/components/InputGroup/InputAddon.vue
@@ -0,0 +1,33 @@
+<template>
+    <span class="fd-input-group__addon" :class="classes">
+        <slot v-if="!props.button"></slot>
+        <button
+            v-if="props.button"
+            class="fd-input-group__button fd-button fd-button--transparent"
+        >
+            <slot></slot>
+        </button>
+    </span>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    compact: {
+        type: Boolean,
+        default: false
+    },
+    button: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const classes = computed(() => {
+    return {
+        'fd-input-group__addon--compact': props.compact,
+        'fd-input-group__addon--button': props.button
+    };
+});
+</script>
