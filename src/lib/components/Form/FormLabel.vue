0574a60410f45cde7893af7c8be218b96bdc6f78
Sajad Abasi <sab3053@gmail.com>
2023-05-26 17:27:05 +0330

    create form label component

---
+++ src/lib/components/Form/FormLabel.vue
@@ -0,0 +1,26 @@
+<template>
+    <span
+        class="fd-form-label"
+        :aria-required="String(required)"
+        :class="classes"
+    >
+        <slot />
+    </span>
+</template>
+
+<script setup>
+import { computed, defineProps } from 'vue';
+
+const props = defineProps({
+    required: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const classes = computed(() => {
+    return {
+        'fd-form-label--required': props.required
+    };
+});
+</script>
