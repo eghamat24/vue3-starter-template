c83419aac2fb139ff9ba33a1c318f28d0f019646
Sajad Abasi <sab3053@gmail.com>
2023-05-26 17:37:06 +0330

    delete prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,8 +5,7 @@ module.exports = {
     root: true,
     extends: [
         'plugin:vue/vue3-essential',
-        'eslint:recommended',
-        '@vue/eslint-config-prettier'
+        'eslint:recommended'
     ],
     parserOptions: {
         ecmaVersion: 'latest'
