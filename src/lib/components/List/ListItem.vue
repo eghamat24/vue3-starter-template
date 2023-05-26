4b2e48fc0b8b5e721458152ceff81587be887808
Sajad Abasi <sab3053@gmail.com>
2023-05-26 17:27:54 +0330

    create list item component

---
+++ src/lib/components/List/ListItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <li
+        class="fd-list__item"
+        tabindex="0"
+        :class="classes"
+        :aria-selected="String(selected)"
+        role="option"
+    >
+        <slot />
+
+        <template v-if="$slots['secondary-title']">
+            <div class="fd-list__secondary">
+                <slot name="secondary-title"></slot>
+            </div>
+        </template>
+    </li>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    selected: {
+        type: Boolean,
+        default: false
+    },
+    active: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const classes = computed(() => {
+    const { active } = props;
+    return {
+        'is-active': active
+    };
+});
+</script>
