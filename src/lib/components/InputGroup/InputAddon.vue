b488ededc559ab4989837b76135e8309103981e8
Sajad Abasi <sab3053@gmail.com>
2023-05-26 17:38:35 +0330

    refactor

--- src/lib/components/InputGroup/InputAddon.vue
+++ src/lib/components/InputGroup/InputAddon.vue
@@ -1,13 +1,13 @@
 <template>
-    <span class="fd-input-group__addon" :class="classes">
-        <slot v-if="!props.button"></slot>
-        <button
-            v-if="props.button"
-            class="fd-input-group__button fd-button fd-button--transparent"
-        >
-            <slot></slot>
-        </button>
-    </span>
+<span class="fd-input-group__addon" :class="classes">
+    <slot v-if="!props.button"></slot>
+    <button
+        v-if="props.button"
+        class="fd-input-group__button fd-button fd-button--transparent"
+    >
+        <slot></slot>
+    </button>
+</span>
 </template>
 
 <script setup>
