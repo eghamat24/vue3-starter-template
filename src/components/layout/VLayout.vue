cf3b414e1f6e43e2c2fbab70bef996ba777ae3bb
masoud <masoudmotahari1380@gmail.com>
2023-07-29 16:41:32 +0330

    feat(components) : add layout components

---
+++ src/components/layout/VLayout.vue
@@ -0,0 +1,34 @@
+<template>
+    <slot name="header">
+        <VHeader/>
+    </slot>
+
+    <main class="row g-0 flex-fill">
+        <slot name="side-bar">
+            <VSideBar />
+        </slot>
+
+        <div class="col bg-light p-4">
+            <slot>Content</slot>
+        </div>
+    </main>
+
+    <slot name="footer">
+        <VFooter />
+    </slot>
+</template>
+
+<script>
+    // Components
+    import VHeader from '@/components/layout/VHeader.vue';
+    import VFooter from '@/components/layout/VFooter.vue';
+    import VSideBar from '@/components/layout/VSideBar.vue';
+
+    export default {
+        components: {
+            VHeader,
+            VSideBar,
+            VFooter
+        }
+    }
+</script>
