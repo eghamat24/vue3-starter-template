9cc21db24328302c8c94428a194e7dce9f8517f9
masoud <masoudmotahari1380@gmail.com>
2023-07-29 15:18:35 +0330

    feat: add bootstrap and rtlcss

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,6 @@
 import ApiService from '@/services/api.service';
 import TokenService from '@/services/token.service';
+import LanguageService from '@/services/language.service';
 
 import AuthenticateUser from '@/services/middleware/AuthenticateUser';
 
@@ -9,3 +10,9 @@ import MimeType from '@/enums/MimeType';
 ApiService.setHeader(HttpHeader.AUTHORIZATION, `Bearer ${TokenService.get()}`);
 ApiService.setHeader(HttpHeader.CONTENT_TYPE, MimeType.APPLICATION_JSON);
 ApiService.addResponseMiddleware(AuthenticateUser);
+
+if (LanguageService.isRtl()) {
+    import('@/assets/scss/app-rtl.scss');
+} else {
+    import('@/assets/scss/app-ltr.scss');
+}
