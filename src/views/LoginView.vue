f708fc1342a6b3f73289948570b10a25ed359629
AliAlizadeh <alizade.dev@gmail.com>
2023-11-15 16:03:25 +0330

    [No Task ID] initial datepicker component v1.0.0

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,17 +1,30 @@
 <template>
-    <header>
-        <nav>
-            <RouterLink :to="{ name: 'Dashboard' }">
-                {{ $t('Dashboard') }}
-            </RouterLink>
-            |
-            <RouterLink :to="{ name: 'Login' }">
-                {{ $t('Login') }}
-            </RouterLink>
-        </nav>
-    </header>
+<header>
+    <nav>
+        <RouterLink :to="{ name: 'Dashboard' }">
+            {{ $t('Dashboard') }}
+        </RouterLink>
+        |
+        <RouterLink :to="{ name: 'Login' }">
+            {{ $t('Login') }}
+        </RouterLink>
+    </nav>
+</header>
 
-    <main>
-        <h1>{{ $t('Login') }}</h1>
-    </main>
+<main>
+    <h1>{{ $t('Login') }}</h1>
+    <VDatepicker month="11" year="2023" @select-date="(date)=> selectedDate = date"/>
+    <br/>
+    <div>
+        selected Date: {{ selectedDate }}
+    </div>
+</main>
 </template>
+<script setup>
+import VDatepicker from "@/components/datepicker/VDatepicker.vue";
+import {ref} from "vue";
+
+const selectedDate = ref('');
+
+
+</script>
