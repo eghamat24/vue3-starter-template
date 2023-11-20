3b094a5fe5693b4c01d1509a2c52eb2b877f10a7
AliAlizadeh <alizade.dev@gmail.com>
2023-11-20 12:46:59 +0330

    init language

--- src/locales/en/messages.js
+++ src/locales/en/messages.js
@@ -3,4 +3,7 @@ export default {
     "Dashboard": "Dashboard",
     "Authorization Error": "Authorization Error",
     "No data available": "No data available",
+    "Calendar": "calendar",
+    "Jalali": "jalali",
+    "Gregorian": "gregorian",
 };
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -13,7 +13,7 @@
 
 <main>
     <h1>{{ $t('Login') }}</h1>
-    <VDatepicker  :min="new Date(2023,10,18)"  year="2023" month="11" @select-date="(date)=> selectedDate = date"/>
+    <VDatepicker :min="new Date(2023,10,18)" year="2023" month="11" @select-date="(date)=> selectedDate = date"/>
     <br/>
     <div>
         selected Date: {{ selectedDate }}
@@ -24,6 +24,7 @@
 <script setup>
 import VDatepicker from "@/components/datepicker/VDatepicker.vue";
 import {ref} from "vue";
+
 const selectedDate = ref('');
 
 
