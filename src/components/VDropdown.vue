9d9f6ecf8e163f0ffad61399240e2a1cf0e5efb3
AliAlizadeh <alizade.dev@gmail.com>
2023-11-26 13:27:20 +0330

    edit dropdown

--- src/components/VDropdown.vue
+++ src/components/VDropdown.vue
@@ -1,180 +1,199 @@
 <template>
-    <div class="dropdown">
-        <div
-            ref="activatorContainer"
-            class="dropdown-activator"
-        >
-            <slot
-                :on="listeners"
-                :show="isShown"
-                name="activator"
-            ></slot>
-        </div>
-
-
-        <Teleport to="body">
-            <div
-                ref="dropdownMenu"
-                :class="dropdownMenuClassNames"
-            >
-                <slot :hide="hide"></slot>
-            </div>
-        </Teleport>
-
+<div class="dropdown">
+    <div
+        ref="activatorContainer"
+        class="dropdown-activator d-inline-block"
+    >
+        <slot
+            :on="listeners"
+            :show="isShown"
+            name="activator"
+        ></slot>
     </div>
+
+
+    <Teleport to="body">
+        <div
+            v-if="isShown"
+            ref="dropdownMenu"
+            :class="dropdownMenuClassNames">
+            <slot :hide="hide"></slot>
+        </div>
+    </Teleport>
+
+</div>
 </template>
 
 <script>
-    import { computed, nextTick, ref } from 'vue';
+import {computed, nextTick, ref} from 'vue';
 
-    // Enums
-    import ComponentPosition from '../enums/ComponentPosition';
-    import ComponentTrigger from "../enums/ComponentTrigger";
+// Enums
+import ComponentPosition from '../enums/ComponentPosition';
+import ComponentTrigger from "../enums/ComponentTrigger";
 
-    // Services
-    import LanguageService from '@/services/language.service';
+// Services
+import LanguageService from '@/services/language.service';
 
-    // Composable
-    import { onClickOutside } from '@/composables/click.composable';
+// Composable
+import {onClickOutside} from '@/composables/click.composable';
 
-    export default {
-        name: 'VDropdown',
+export default {
+    name: 'VDropdown',
 
-        props: {
-            position: {
-                type: String,
-                default: ComponentPosition.BOTTOM,
-                validator(value) {
-                    return Object.values(ComponentPosition).includes
-                    (value);
-                }
-            },
-            trigger: {
-                type: String,
-                default: ComponentTrigger.CLICK,
-                validator(value) {
-                    return Object.values(ComponentTrigger).includes(value);
-                }
+    props: {
+        position: {
+            type: String,
+            default: ComponentPosition.BOTTOM,
+            validator(value) {
+                return Object.values(ComponentPosition).includes
+                (value);
             }
         },
+        trigger: {
+            type: String,
+            default: ComponentTrigger.CLICK,
+            validator(value) {
+                return Object.values(ComponentTrigger).includes(value);
+            }
+        }
+    },
 
-        setup(props) {
-            const listeners = computed(() => {
-                const result = {};
+    setup(props) {
+        const listeners = computed(() => {
+            const result = {};
 
-                switch (props.trigger) {
-                    case ComponentTrigger.CLICK:
-                        result.click = toggle;
-                        break;
-                    case ComponentTrigger.HOVER:
-                        result.mouseenter = show;
-                        result.mouseleave = hide;
-                        break;
-                    case ComponentTrigger.FOCUS:
-                        result.focus = show;
-                        result.blur = hide;
-                        break;
-                }
-
-                return result;
-            });
-
-            const dropdownMenuClassNames = computed(() => {
-                return {
-                    'dropdown-menu position-fixed py-0 overflow-auto': true,
-                    'show': isShown.value
-                };
-            });
-
-
-            const isShown = ref(false);
-            const dropdownMenu = ref();
-            const activatorContainer = ref();
-
-            let offCloseHandler;
-
-            function show() {
-                isShown.value = true;
-
-                const rect = activatorContainer.value.getBoundingClientRect();
-
-                const { off } = onClickOutside(dropdownMenu, function () {
-                    hide();
-                });
-
-                offCloseHandler = off;
-                nextTick(() => {
-                    switch (props.position) {
-                        case ComponentPosition.TOP:
-                            dropdownMenu.value.style.top = rect.top + window.scrollY - dropdownMenu.value.clientHeight + 'px';
-                            if (LanguageService.isRtl()) {
-                                dropdownMenu.value.style.left = rect.left + 'px';
-                            } else {
-                                dropdownMenu.value.style.left = rect.left - activatorContainer.value.clientWidth - activatorContainer.value.clientWidth + 'px';
-                            }
-                            break;
-
-                        case ComponentPosition.BOTTOM:
-                            dropdownMenu.value.style.top = rect.top + rect.height + 'px';
-                            if (LanguageService.isRtl()) {
-                                dropdownMenu.value.style.left = rect.left + 'px';
-                            } else {
-                                dropdownMenu.value.style.left = rect.left - activatorContainer.value.clientWidth - activatorContainer.value.clientWidth + 'px';
-                            }
-                            break;
-
-                        case ComponentPosition.START:
-                            dropdownMenu.value.style.top = rect.top + window.scrollY + 'px';
-
-                            if (LanguageService.isRtl()) {
-                                dropdownMenu.value.style.left = rect.left + rect.width + 10 + 'px';
-                            } else {
-                                dropdownMenu.value.style.left = rect.left - dropdownMenu.value.clientWidth + 'px';
-                            }
-
-                            break;
-
-                        case ComponentPosition.END:
-                            dropdownMenu.value.style.top = rect.top + window.scrollY + 'px';
-
-                            if (LanguageService.isRtl()) {
-                                dropdownMenu.value.style.left = rect.left - dropdownMenu.value.clientWidth + 'px';
-                            } else {
-                                dropdownMenu.value.style.left = rect.left + rect.width + 'px';
-                            }
-
-                            break;
-                    }
-                });
+            switch (props.trigger) {
+                case ComponentTrigger.CLICK:
+                    result.click = toggle;
+                    break;
+                case ComponentTrigger.HOVER:
+                    result.mouseenter = show;
+                    result.mouseleave = hide;
+                    break;
+                case ComponentTrigger.FOCUS:
+                    result.focus = show;
+                    result.blur = hide;
+                    break;
             }
 
-            function hide() {
-                isShown.value = false;
-                offCloseHandler();
-            }
+            return result;
+        });
 
-            function toggle(event) {
-                if (isShown.value === false) {
-                    show(event);
-                } else {
-                    hide();
-                }
-            }
+        const dropdownMenuClassNames = computed(() => {
 
             return {
-                dropdownMenuClassNames,
-
-                isShown,
-
-                listeners,
-
-                dropdownMenu,
-                activatorContainer,
-
-                show,
-                hide
+                'dropdown-menu position-fixed py-0 overflow-auto': true,
+                'show': isShown.value
             };
+        });
+
+
+        const isShown = ref(false);
+        const dropdownMenu = ref();
+        const activatorContainer = ref();
+
+        let offCloseHandler;
+
+        function show() {
+            isShown.value = true;
+
+            const rect = activatorContainer.value.getBoundingClientRect();
+            const vpWidth = window.innerWidth || document.documentElement.clientWidth;
+            const vpHeight = window.innerHeight || document.documentElement.clientHeight;
+
+            const {off} = onClickOutside(dropdownMenu, function () {
+                hide();
+            });
+
+            offCloseHandler = off;
+            nextTick(() => {
+
+
+                let dropdown = null;
+
+                switch (props.position) {
+                    case ComponentPosition.TOP:
+                        dropdownMenu.value.style.top = rect.top + window.scrollY - dropdownMenu.value.clientHeight + 'px';
+                        if (LanguageService.isRtl()) {
+                            dropdownMenu.value.style.left = rect.left + 'px';
+                        } else {
+                            dropdownMenu.value.style.left = rect.left - activatorContainer.value.clientWidth - activatorContainer.value.clientWidth + 'px';
+                        }
+                        break;
+
+                    case ComponentPosition.BOTTOM:
+                        dropdownMenu.value.style.top = rect.top + rect.height + 'px';
+                        if (LanguageService.isRtl()) {
+                            dropdownMenu.value.style.left = rect.left + 'px';
+
+                            dropdown = dropdownMenu.value.getBoundingClientRect();
+                            if ((dropdown.width + dropdown.left) > vpWidth) {
+                                dropdownMenu.value.style.right = activatorContainer.value.clientWidth  + 'px';
+                                dropdownMenu.value.style.left = 'initial';
+                            }
+                            if ((dropdown.height + dropdown.top) > vpHeight) {
+                                dropdownMenu.value.style.top = 'initial';
+                                dropdownMenu.value.style.bottom = '0px';
+                            }
+
+                        } else {
+                            // dropdownMenu.value.style.left = rect.left - activatorContainer.value.clientWidth + 'px';
+                            dropdownMenu.value.style.left = rect.left + 'px';
+                        }
+                        break;
+
+                    case ComponentPosition.START:
+                        dropdownMenu.value.style.top = rect.top + window.scrollY + 'px';
+
+                        if (LanguageService.isRtl()) {
+                            dropdownMenu.value.style.left = rect.left + rect.width + 10 + 'px';
+                        } else {
+                            dropdownMenu.value.style.left = rect.left + 'px';
+                        }
+
+                        break;
+
+                    case ComponentPosition.END:
+                        dropdownMenu.value.style.top = rect.top + window.scrollY + 'px';
+
+                        if (LanguageService.isRtl()) {
+                            dropdownMenu.value.style.left = rect.left - dropdownMenu.value.clientWidth + 'px';
+                        } else {
+                            dropdownMenu.value.style.left = rect.left + rect.width + 'px';
+                        }
+
+                        break;
+                }
+            });
         }
 
-    };
+        function hide() {
+            isShown.value = false;
+            offCloseHandler();
+        }
+
+        function toggle(event) {
+            if (isShown.value === false) {
+                show(event);
+            } else {
+                hide();
+            }
+        }
+
+        return {
+            dropdownMenuClassNames,
+
+            isShown,
+
+            listeners,
+
+            dropdownMenu,
+            activatorContainer,
+
+            show,
+            hide
+        };
+    }
+
+};
 </script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -13,17 +13,36 @@
 
 <main>
     <h1>{{ $t('Login') }}</h1>
-    <div class="btn btn-info" @click="()=>selectedDate='2023/11/5'">Set 2023 Nov 5</div>
-    <div>{{ selectedDate }}</div>
+
+
+    <VDropdown position="bottom">
+        <template #activator="{on}">
+            <div v-on="on">test</div>
+        </template>
+        <template #default>
+            <div>menu1</div>
+            <div>menu2</div>
+            <div>menu3</div>
+            <div>menu4</div>
+            <div>menu5</div>
+            <div>menu6</div>
+            <div>menu7</div>
+        </template>
+    </VDropdown>
+
+
+    <!--    <div class="btn btn-info" @click="()=>selectedDate='2023/11/5'">Set 2023 Nov 5</div>-->
+    <!--    <div>{{ selectedDate }}</div>-->
     <!--    <VDatepicker :min="new Date(2023,10,18)" year="2023" month="11" @select-date="(date)=> selectedDate = date"/>-->
-    <VDatepicker year="2023" month="11" v-model:selected="selectedDate"/>
-    <br/>
+    <!--    <VDatepicker year="2023" month="11" v-model:selected="selectedDate"/>-->
+    <!--    <br/>-->
 </main>
 </template>
 <script setup>
 import VDatepicker from "@/components/datepicker/VDatepicker.vue";
 import {ref} from "vue";
 import Pasoonate from "pasoonate/dist/Pasoonate";
+import VDropdown from "@/components/VDropdown.vue";
 
 const selectedDate = ref(Pasoonate.make().gregorian().format('yyyy/M/dd'));
 
