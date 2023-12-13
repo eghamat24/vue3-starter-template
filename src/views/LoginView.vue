a523cb365b0892c5110180a225556b5278617d32
AliAlizadeh <alizade.dev@gmail.com>
2023-12-13 13:31:24 +0330

    fix(VDatePicker): remove datepicker for commit it on separate branch later

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -13,18 +13,5 @@
 
 <main>
     <h1>{{ $t('Login') }}</h1>
-    <div class="btn btn-info" @click="()=>selectedDate='2023/11/5'">Set 2023 Nov 5</div>
-    <div>{{ selectedDate }}</div>
-    <VDatepicker year="2023" month="11" v-model:selected="selectedDate"/>
-    <br/>
 </main>
 </template>
-<script setup>
-import VDatepicker from "@/components/datepicker/VDatepicker.vue";
-import {ref} from "vue";
-import Pasoonate from "pasoonate/dist/Pasoonate";
-
-const selectedDate = ref(Pasoonate.make().gregorian().format('yyyy/M/dd'));
-
-
-</script>
