90fe9f64d135abbe4cde3061db0cff435baef304
no2ehi <graphist77@gmail.com>
2023-12-28 15:26:16 +0330

    feat(LoginView.vue): ADD demo test Bottom Sheet Component

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -14,7 +14,6 @@
 <main>
     <h1>{{ $t('Login') }}</h1>
 
-
     <div class="d-flex flex-column align-items-center justify-content-center">
         <button id="btn-modal" @click="showModal = !showModal">show Modal show with Model Value</button>
         <br>
@@ -35,6 +34,56 @@
             footer modal
         </template>
     </VModal>
+
+    <div class="d-flex flex-column align-items-center justify-content-center mt-4">
+        <button id="btn-modal" @click="showModalBottomSheet = !showModalBottomSheet">show Modal Bottom Sheet show with Model Value</button>
+        <br>
+        <button id="btn-modal-bottom-sheet" v-modal-directive.modal-bottom-sheet>show Modal Bottom Sheet with directive (pin)</button>
+    </div>
+
+    <VModalBottomSheet
+        id="modal-bottom-sheet"
+        v-model:show="showModalBottomSheet"
+    >
+        <template #title>
+            title modal bottom sheet
+        </template>
+
+        <template #body>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aperiam culpa cum fugiat nemo quae? Aliquid beatae, cupiditate
+            facilis hic ipsa labore laudantium molestiae odio perspiciatis placeat, quisquam tempora ut.Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. A aperiam culpa cum fugiat nemo quae? Aliquid beatae, cupiditate
+            facilis hic ipsa labore laudantium molestiae odio perspiciatis placeat, quisquam tempora ut.
+        </template>
+
+        <template #footer>
+            footer modal
+        </template>
+    </VModalBottomSheet>
+
+    <div class="d-flex flex-column align-items-center justify-content-center mt-4">
+        <button @click="showBottomSheet = !showBottomSheet">show Bottom Sheet show with Model Value</button>
+    </div>
+
+    <VBottomSheet
+        id="bottom-sheet"
+        v-model:show="showBottomSheet"
+    >
+        <template #title>
+            title bottom sheet
+        </template>
+
+        <template #body>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aperiam culpa cum fugiat nemo quae? Aliquid beatae, cupiditate
+            facilis hic ipsa labore laudantium molestiae odio perspiciatis placeat, quisquam tempora ut.Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. A aperiam culpa cum fugiat nemo quae? Aliquid beatae, cupiditate
+            facilis hic ipsa labore laudantium molestiae odio perspiciatis placeat, quisquam tempora ut.
+        </template>
+
+        <template #footer>
+            footer modal
+        </template>
+    </VBottomSheet>
 </main>
 </template>
 
@@ -44,9 +93,13 @@ import { ref } from "vue";
 
 // Components
 import VModal from "@/components/VModal.vue";
+import VModalBottomSheet from "@/components/VModalBottomSheet.vue";
+import VBottomSheet from "@/components/VBottomSheet.vue";
 
 // Directive
 import { modal as vModalDirective } from "@/directives/modal.directive";
 
 const showModal = ref(false);
+const showModalBottomSheet = ref(false);
+const showBottomSheet = ref(false);
 </script>
