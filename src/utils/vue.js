449a7489a90f8a44bf4764d3c1b41de4bf2929de
Seyyed Iman Asnafi <s.imanasnafi@gmail.com>
2024-01-05 19:31:42 +0330

    refactor(Utils): Use hasOwnProperty Method

--- src/utils/vue.js
+++ src/utils/vue.js
@@ -1,5 +1,8 @@
+// Utils
+import { hasOwnProperty } from '@/utils/index';
+
 function hasVNodeSlot(vnode, slotName) {
-    return vnode.children !== null && Object.prototype.hasOwnProperty.call(vnode.children, slotName);
+    return vnode.children !== null && hasOwnProperty(vnode.children, slotName);
 }
 
 export {
